f1b3581673b71af969d94161054a53d108f2d49b
Musa <musakarakilic@gmail.com>
2023-06-18 19:55:31 +0300

    last changes

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,14 +1,13 @@
 'use client'
 
-import { User } from '@prisma/client'
-
 import Container from "../Container"
 import Logo from "./Logo"
 import Search from "./Search"
 import UserMenu from "./UserMenu"
+import { SafeUser } from '@/app/types'
 
 interface NavbarProps {
-  currentUser?: User | null
+  currentUser?: SafeUser | null
 }
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
@@ -20,7 +19,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
-            <UserMenu currentUser= { currentUser} />
+            <UserMenu currentUser= { currentUser } />
           </div>
         </Container>
         </div>
