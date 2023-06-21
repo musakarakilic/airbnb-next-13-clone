7c9bbed2c99729424ab780871e4fa59923172f38
Musa <musakarakilic@gmail.com>
2023-06-22 00:54:26 +0300

    new feature added

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,6 +6,7 @@ import { useCallback, useState } from "react"
 import MenuItem from "./MenuItem"
 import useRegisterModal from "@/app/hooks/useRegisterModal"
 import useLoginModal from "@/app/hooks/useLoginModal"
+import useRentModal from "@/app/hooks/useRentModal"
 import { signOut } from "next-auth/react"
 import { SafeUser } from "@/app/types"
 
@@ -16,17 +17,28 @@ interface UserMenuProps {
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     const registerModal = useRegisterModal()
     const loginModal = useLoginModal()
+    const rentModal = useRentModal()
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value)
     },[])
+
+    const onRent = useCallback(() => {
+        if(!currentUser){
+            return loginModal.onOpen()
+        }
+
+        // Open rent Modal
+        rentModal.onOpen()
+
+    }, [currentUser, loginModal, rentModal])
   return (
     <div className="relative">
         <div className="flex flex-row items-center gap-3">
 
             <div
-            onClick={() => {}}
+            onClick={onRent}
             className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
             >
                 Airbnb your home
@@ -66,7 +78,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                     label="My properties"
                     />
                     <MenuItem 
-                    onClick={() => {}}
+                    onClick={rentModal.onOpen}
                     label="Airbnb my home"
                     />
                     <hr />
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar/Navbar"
 import ClientOnly from "./components/ClientOnly"
 import RegisterModal from "./components/modals/RegisterModal"
 import LoginModal from "./components/modals/LoginModal"
+import RentModal from "./components/modals/RentModal"
 
 import ToasterProvider from "./providers/ToasterProvider"
 import getCurrentUser from "./actions/getCurrentUser"
@@ -30,6 +31,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <RentModal />
           <RegisterModal />
           <LoginModal />
           <Navbar currentUser={currentUser}/>
